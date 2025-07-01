5543a2c22d05e021d40dbf7d813f89bb0839e2da
mingyun0809 <mingyun0809>
2025-07-01 17:38:10 +0900

    0701 commit

---
+++ src/main/java/com/mgk/studytracker/entities/UserEntity.java
@@ -0,0 +1,14 @@
+package com.mgk.studytracker.entities;
+
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class UserEntity {
+
+}
